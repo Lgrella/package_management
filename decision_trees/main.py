5cdf4b856c85ad7ae23e50f002b7d9558f237886
Nikhil Sinha <nikhilsinha@outlook.com>
2024-11-11 17:26:25 -0500

    Base setup

---
+++ decision_trees/main.py
@@ -0,0 +1,8 @@
+### IMPORTS
+import pandas as pd
+from core.main import Model
+###
+
+class DecisionTree(Model):
+    def __init__(self, params):
+        super().__init__(params)
---
+++ logistic_regression/main.py
@@ -0,0 +1,8 @@
+### IMPORTS
+import pandas as pd
+from core.main import Model
+###
+
+class LinearRegression(Model):
+    def __init__(self, params):
+        super().__init__(params)
