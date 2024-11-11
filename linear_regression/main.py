ad462cd7f499362bfd966db05b2cf71514101821
Nikhil Sinha <nikhilsinha@outlook.com>
2024-11-11 17:45:09 -0500

    LinReg

--- linear_regression/main.py
+++ linear_regression/main.py
@@ -1,8 +1,18 @@
 ### IMPORTS
 import pandas as pd
-from core.main import Model
+import numpy as np
+from core.main import SGDModel
 ###
 
-class LinearRegression(Model):
+class LinearRegression(SGDModel):
     def __init__(self, params):
         super().__init__(params)
+
+    def loss(self, data, targets):
+        preds = self.predict(data)
+        return np.mean(np.square(preds-targets), axis=1)
+    
+    def predict(self, data):
+        W = self.params["W"]
+        b = self.params["b"]
+        return np.matmul(W, data) + b
