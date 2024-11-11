db900a5dd6572072997dc4c8355f7f6727e6c042
Nikhil Sinha <nikhilsinha@outlook.com>
2024-11-11 17:30:12 -0500

    Some more setup

--- core/main.py
+++ core/main.py
@@ -1,7 +1,16 @@
 ### IMPORTS
 import pandas as pd
+from abc import ABC, abstractmethod
 ###
 
-class Model:
+class Model(ABC):
     def __init__(self, params):
         self.params = params
+
+    @abstractmethod
+    def train(self, data):
+        pass
+
+    @abstractmethod
+    def predict(self, data):
+        pass
